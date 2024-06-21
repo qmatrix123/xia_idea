e38d3ab0ca5c434cd289f338d411ad12a19864c9
qmatrix123 <qmatrix123@163.com>
2024-06-21 11:21:38 +0800

    v1

--- app.py
+++ app.py
@@ -1,44 +1,51 @@
-from st_pages import hide_pages
-from time import sleep
 import streamlit as st
+from time import sleep
+import base64
 
+# 编码函数
+def encrypt_employee_id(employee_id):
+    encrypted_id = base64.b64encode(employee_id.encode()).decode()
+    return encrypted_id
+
+# 解码函数
+def decrypt_employee_id(encrypted_id):
+    decrypted_id = base64.b64decode(encrypted_id.encode()).decode()
+    return decrypted_id
 
 def log_in():
     st.session_state["logged_in"] = True
-    hide_pages([])
     st.success("Logged in!")
     sleep(0.5)
     st.switch_page("pages/user.py")
-
 
 def log_out():
     st.session_state["logged_in"] = False
     st.success("Logged out!")
     sleep(0.5)
 
-
 if "id" in st.query_params:
-    st.session_state["id"] = st.query_params["id"]
+    encrypted_username = st.query_params["id"]
+    st.session_state["id"] = encrypted_username
     st.session_state["logged_in"] = True
     st.switch_page("pages/user.py")
 
-if not st.session_state.get("logged_in", False):
-    hide_pages(["page1", "page2", "page3"])
-    username = st.text_input("Username", key="username")
-    password = st.text_input("Password", key="password", type="password")
+username = st.text_input("员工编号", key="username")
 
-    if username == "test" and password == "test":
-        st.session_state["logged_in"] = True
-        st.session_state["id"] = "test"
-        st.query_params["id"] = "test"
-        hide_pages([])
-        st.success("Logged in!")
-        sleep(0.5)
-        st.switch_page("pages/user.py")
+if st.button("登录", key="login_button", help="点击此按钮以登录", use_container_width=True):
+    if not st.session_state.get("logged_in", False):
+        if username != "" and len(username) == 7:
+            # 加密员工编号
+            encrypted_username = encrypt_employee_id(username)
 
-else:
-    st.write("Logged in!")
-    st.button("log out", on_click=log_out)
+            st.session_state["logged_in"] = True
+            st.session_state["id"] = encrypted_username
+            st.query_params["id"] = encrypted_username
+            st.success("Logged in!")
+            sleep(0.5)
+            st.switch_page("pages/user.py")
+    else:
+        st.write("Logged in!")
+        st.button("log out", on_click=log_out)
 
 if "id" in st.session_state and st.session_state["id"] == "test":
     st.session_state["logged_in"] = True
--- pages/user.py
+++ pages/user.py
@@ -1,9 +1,14 @@
 import streamlit as st
-import pandas as pd
 from datetime import datetime
 from sqlalchemy import create_engine, Column, Integer, String, DateTime
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
+import base64
+
+# 解码函数
+def decrypt_employee_id(encrypted_id):
+    decrypted_id = base64.b64decode(encrypted_id.encode()).decode()
+    return decrypted_id
+
 
 # 数据库连接字符串
 DATABASE_URI = 'postgresql://idea_owner:K7et2ERyJBqk@ep-tight-paper-a1dw4ef8.ap-southeast-1.aws.neon.tech/idea?sslmode=require'
@@ -20,59 +25,54 @@ class Idea(Base):
     timestamp = Column(DateTime, default=datetime.now)
     title = Column(String)
     description = Column(String)
-    employee_name = Column(String)
     employee_id = Column(String)
-    department = Column(String)
+    status = Column(String)
+    remark = Column(String)
 
 # 创建数据库表（如果不存在）
 Base.metadata.create_all(engine)
 
-# 创建一个空的DataFrame来存储想法
-ideas_df = pd.DataFrame(columns=['Timestamp', 'Title', 'Description', 'Employee Name', 'Employee ID', 'Department'])
-
 # 定义展示想法页面
 def show_ideas_page():
-    st.title('员工想法提交系统')
+    st.title('小想法，大智慧')
 
     st.header('提交新想法')
-    title = st.text_input('想法标题')
-    description = st.text_area('想法描述')
-    employee_name = st.text_input('员工姓名')
-    employee_id = st.session_state["id"]
-    department = st.selectbox('相关部门', ['技术部门', '市场部门', '生产部门', '财务部门'])
+    title = st.text_input('标题')
+    description = st.text_area('描述')
+    employee_id = decrypt_employee_id(st.session_state.get("id"))
 
     col1, col2, col3 = st.columns([1, 3, 1])
     with col2:
         if st.button('提交想法', key='submit_button', help='提交您的想法', use_container_width=True):
             timestamp = datetime.now()
-            new_row = {'Timestamp': timestamp, 'Title': title, 'Description': description,
-                        'Employee Name': employee_name, 'Employee ID': employee_id,
-                        'Department': department}
-            ideas_df.loc[len(ideas_df)] = new_row
+            new_idea = Idea(timestamp=timestamp, title=title, description=description,
+                            employee_id=employee_id, status='待处理', remark='')
 
             session = Session()
-            new_idea = Idea(timestamp=timestamp, title=title, description=description,
-                            employee_name=employee_name, employee_id=employee_id, department=department)
             session.add(new_idea)
             session.commit()
             session.close()
 
             st.success('您的想法已成功提交, 管理员稍后会尽快处理!')
+
     # 从数据库中加载已提交的想法
     session = Session()
-    submitted_ideas = session.query(Idea).filter_by(employee_id=employee_id).all()
+    submitted_ideas = session.query(Idea).filter_by(employee_id=employee_id).order_by(Idea.timestamp.desc()).all()
     session.close()
 
     if submitted_ideas:
-        submitted_ideas_df = pd.DataFrame([{
-            'Timestamp': idea.timestamp,
-            'Title': idea.title,
-            'Description': idea.description,
-            'Employee Name': idea.employee_name,
-            'Employee ID': idea.employee_id,
-            'Department': idea.department
-        } for idea in submitted_ideas])
-        st.dataframe(submitted_ideas_df)
+        st.markdown("<h3></h3>", unsafe_allow_html=True)
+
+        for idea in submitted_ideas:
+            formatted_timestamp = idea.timestamp.strftime('%Y-%m-%d %H:%M:%S')  # 格式化时间戳
+
+            st.write(f"### {idea.title}")
+            st.markdown(f"**描述:** {idea.description}")
+            st.write(f"**状态:** {idea.status}")
+            st.write(f"**备注:** {idea.remark}")
+            st.write(f"**时间:** {formatted_timestamp}")
+            st.write("---")
+
     else:
         st.info('暂无已提交的想法。')
 
@@ -80,11 +80,9 @@ def log_out():
     st.session_state["logged_in"] = False
     del st.session_state.id
     del st.query_params.id
-    
 
-if "logged_in" not in st.session_state or  st.session_state["logged_in"] == False:
+if "logged_in" not in st.session_state or not st.session_state["logged_in"]:
     st.switch_page("app.py")
-
 
 if "id" in st.session_state:
     st.query_params["id"] = st.session_state["id"]
